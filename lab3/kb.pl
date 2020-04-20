ba65bd857a1cb83247a01511688ad6c5d0f8ec93
chiaz <chiayu.dev@gmail.com>
2020-04-20 18:22:49 +0800

    lab 3 version 2

--- lab3/kb.pl
+++ lab3/kb.pl
@@ -1,13 +1,15 @@
 :- dynamic like/1.
 :- dynamic dislike/1.
+:- dynamic exclude/1.
 
 % facts (menu)
+exclude([]).
 meal([vegan, value, healthy]).
 sandwich([wheat, oat, italian, wrap]).
 meat([fish, chicken, beef, pork]).
 top_up([tuna, cheese, egg_mayo, bacon]).
-salad([lettuce,tomato,cucumbers,olive,onion]).
-sauce([chilli,sweet_onion, mustard, mayonnaise, bbq, ketchup]).
+salad([lettuce, tomato,cucumbers,olive,onion]).
+sauce([chilli, sweet_onion, mustard, mayonnaise, bbq, ketchup]).
 sides([soup, cookie]).
 
 /*
@@ -15,10 +17,13 @@ sides([soup, cookie]).
 	healthy meal -> no fatty sauces
 	value meal -> no top-up
 */
-non_vegan([fish, chicken, beef, pork, onion, tuna, cheese, bacon, sweet_onion]).
-non_healthy([mayonnaise, bbq, ketchup]).
-non_value([tuna, cheese, egg_mayo, bacon]).
-
+should_exclude(vegan, [fish, chicken, beef, pork, onion, tuna, cheese, bacon, sweet_onion]).
+should_exclude(healthy, [mayonnaise, bbq, ketchup]).
+should_exclude(value, [tuna, cheese, egg_mayo, bacon]).
+% meat and sandwish should only allow one selection
+should_exclude(X, L):- 
+	sandwich(L), member(X,L);
+	meat(L), member(X,L).
 
 ask:-
 	gen_options(L), validate_and_query_options(L).
@@ -26,42 +31,26 @@ ask:-
 % generate valid options by subtracting exclusion list from menu list
 gen_options(L):-
 	menu_list(M),
-	exclude_list(E),
-	remaining(M, E, L).
+	%exclude_list(E),
+	exclude(E),
+	subtract(M, E, L).
 	
 validate_and_query_options(L):-
 	% get an option to ask from list
 	member(X,L), 
-	print('Do you like '),	
-	print(X), 
-	print('? y/n/q: '), 
+	write('Do you like '),	
+	write(X), 
+	write('? y/n/q: '), 
 	read(Like), (
 		Like==q -> abort;
-		Like==y -> print('Great..'), assert(like(X));
-		print('Huh..'), assert(dislike(X))
+		Like==y -> (write('Great..'), assert(like(X)), add_exclude([X]), check_exclude(X));
+		write('Huh..'), assert(dislike(X)), add_exclude([X])
 	), 
 	ask;
 	% when an item is failed to get -> L=[] -> order is completed
-	print('Order completed'),
+	write('Order completed'),
 	nl,
 	print_order.
-
-% find remaining element of L1 after subtracting common elements from L2
-remaining(L1, L2, Result):-
-	intersection(L1, L2, Temp), 
-	subtract(L1, Temp, Result).
-
-% construct a exclusion list (history options + item to be excluded based on preferrence)
-exclude_list(L):-
-	% construct history list
-	findall(X, like(X), Likelist),
-	findall(X, dislike(X), Dislikelist),
-	append(Likelist, Dislikelist, History),
-	% adding exclude list
-	(like(vegan), non_vegan(NV); NV=[]), append(History, NV, E1),
-	(like(healthy), non_healthy(NH); NH=[]), append(E1, NH, E2),
-	(like(value), non_value(NV2); NV2=[]), append(E2, NV2, E3),
-	(sandwich(S), member(B,S), like(B), append(E3, S, L); L=E3).
 
 % combine all menu into a list
 menu_list(L):-
@@ -72,15 +61,34 @@ menu_list(L):-
 	append(V3,S2,V4),
 	append(V4,S3,V5),
 	append(V5,S4,L).
+
+check_exclude(X):-
+	like(X),
+	should_exclude(X, L),
+	add_exclude(L).
+
+add_exclude(L):-
+	exclude(E),
+	append(E, L, NewE),
+	retractall(exclude(_)),
+	assert(exclude(NewE)).
 	
 print_order:-
 	meal(M),
 	findall(X, like(X), Likelist),
-	remaining(Likelist, M, L),
-	print(L).
+	subtract(Likelist, M, L),
+	writeln(L).
 	
 clean:-
 	retractall(like(_)),
-	retractall(dislike(_)).
+	retractall(dislike(_)),
+	retractall(exclude(_)),
+	assert(exclude([])).
 	
-	
+log_debug:-
+	findall(X, like(X), L),
+	findall(X, dislike(X), D),
+	exclude(E),
+	write('likes: '),writeln(L),
+	write('dislikes: '),writeln(D),
+	write('excludes: '),writeln(E).
